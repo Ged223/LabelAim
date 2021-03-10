5bef55c60759762ca7b9cecfa3b32e269d2c27c3
Ged223 <gedsvk@gmail.com>
2021-03-10 15:31:21 +0100

    change to Maven from Gradle

---
+++ src/main/java/game/GameApp.java
@@ -0,0 +1,16 @@
+package game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class GameApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
