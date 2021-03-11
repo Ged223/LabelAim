5d01414a2a362bbe79189255032a89f60ad091b0
Ged223 <gedsvk@gmail.com>
2021-03-11 08:00:57 +0100

    set dimensions and title

--- src/main/java/game/GameApp.java
+++ src/main/java/game/GameApp.java
@@ -7,7 +7,10 @@ public class GameApp extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings gameSettings) {
-
+        gameSettings.setWidth(1024);
+        gameSettings.setHeight(576);
+        gameSettings.setTitle("LabelAim");
+        gameSettings.setVersion("0.0");
     }
 
     public static void main(String[] args) {
